797ff533c70cf452d5d5226e18918197994303e2
Rajib Mondal <mondal021002rajib@gmail.com>
2021-06-28 23:37:36 +0530

    Routes setup completed

---
+++ main.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+app.listen(3000);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About Us' });
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Request Service' });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects', { title: 'Our Projects' });
+});
+
+app.use((req, res) => {
+    res.status(404).render('404', { title: 'Wrong URL' });
+});
