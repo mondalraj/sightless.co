d790da44b228374942af3db2a80f8f4916e2fcd6
Rajib Mondal <mondal021002rajib@gmail.com>
2021-06-29 00:16:02 +0530

    ejs setup completed

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { title: 'Home' });
 });
 
 app.get('/about', (req, res) => {
