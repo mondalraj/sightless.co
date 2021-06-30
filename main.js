60d99102b59084da555ea2877d084f8f14092f38
Rajib Mondal <mondal021002rajib@gmail.com>
2021-06-30 20:48:32 +0530

    low fidelity partners and contact section done

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
+    res.render('index', { title: 'Home'});
 });
 
 app.get('/about', (req, res) => {
